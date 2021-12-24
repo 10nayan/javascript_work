37e5f1f9eb11671f6e8e722d01bb25c58c11ec16
Nayan Jyoti Patowary <nayanjyotipatowary@gmail.com>
2021-12-24 13:46:12 +0530

    Total Money restrictions added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded',()=>{
+var totalMoney=parseInt(prompt("How much money do you have?"));
 const button1=document.getElementById('button1');
 const sitton=document.getElementById('Sitton');
 const munda=document.getElementById('Munda');
@@ -13,52 +14,35 @@ button1.addEventListener('click',()=>{
   for (let i=0;i<6;i++){
     noArray.push(getRandomNo());
   }
-  console.log(noArray);
   for (let i of noArray){
     symbolArray.push(roll(i));
   }
-  console.log(symbolArray);
   document.getElementById('image1').innerHTML=`<img src="image/${symbolArray[0]}.png"/>`;
   document.getElementById('image2').innerHTML=`<img src="image/${symbolArray[1]}.png"/>`;
   document.getElementById('image3').innerHTML=`<img src="image/${symbolArray[2]}.png"/>`;
   document.getElementById('image4').innerHTML=`<img src="image/${symbolArray[3]}.png"/>`;
   document.getElementById('image5').innerHTML=`<img src="image/${symbolArray[4]}.png"/>`;
   document.getElementById('image6').innerHTML=`<img src="image/${symbolArray[5]}.png"/>`;
-  var choice=getSignFromId();
-  var money=parseInt(getMoneyFromId());
-  console.log(choice,money);
+  var choice=getSignFromId(idArray);
+  var money=parseInt(getMoneyFromId(idArray));
   const count=elCounter(symbolArray,choice);
-  if (count>1){
+  if (totalMoney<=0){
+    document.querySelector('h3').textContent=`Oops you have lost all your money!`;
+  }
+  else if (totalMoney<money){
+    document.querySelector('h3').textContent=`Oops you dont have that much money!`;
+  }
+  else if (count>1){
     moneyWon=money*count;
-    document.querySelector('h3').textContent=`Congrats You Won ${moneyWon}`;
+    totalMoney=totalMoney+moneyWon;
+    document.querySelector('h3').textContent=`Congrats You Won ${moneyWon}, you have ${totalMoney} now`;
   }else {
-    document.querySelector('h3').textContent=`You lost ${money}`;
+    totalMoney=totalMoney-money;
+    document.querySelector('h3').textContent=`Oops You lost ${money}, you have ${totalMoney} now!`;
   }
+});
+});
 
-});
-console.log("working");
-function getMoneyFromId(){
-  var moneyChosen=0;
-  for (let i of idArray){
-    if (i.value>0){
-      moneyChosen=i.value;
-      i.value="0";
-      break;
-    }
-  }
-  return moneyChosen;
-}
-function getSignFromId(){
-  var signChosen="invalid";
-  for (let i of idArray){
-    if (i.value!=0){
-      signChosen=i.name;
-      break;
-    }
-  }
-  return signChosen;
-}
-});
 function roll(no){
   switch (no) {
     case 1:
@@ -80,9 +64,34 @@ function roll(no){
       return "Rohi";
   }
 }
+
 function elCounter(array,el){
   return array.filter(i=>i===el).length;
 }
+
 function getRandomNo(){
   return Math.floor(Math.random()*6+1);
+}
+
+function getSignFromId(idArray){
+  var signChosen="invalid";
+  for (let i of idArray){
+    if (i.value!=0){
+      signChosen=i.name;
+      break;
+    }
+  }
+  return signChosen;
+}
+
+function getMoneyFromId(idArray){
+  var moneyChosen=0;
+  for (let i of idArray){
+    if (i.value>0){
+      moneyChosen=i.value;
+      i.value="0";
+      break;
+    }
+  }
+  return moneyChosen;
 }
